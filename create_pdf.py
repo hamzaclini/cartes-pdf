32c551fb2a5da105c6ba65d946b8f20bc056ff43
khribechbouchaib <khribech.chouaib@gmail.com>
2024-04-26 15:12:50 +0200

    commit 4
    back to original code for create pdf because the try didnot work

--- create_pdf.py
+++ create_pdf.py
@@ -13,22 +13,15 @@ def create_pdf(cards):
     pdf.add_page()
     pdf["logo"] = "logo.png"
     for index in range(min(8, len(cards))):
-        pdf.set_font("Arial", size=14)
-        pdf.set_xy(pdf.get_x() + (pdf.epw - pdf.get_string_width(cards[index][0])) / 2, pdf.get_y() + 30)
-        pdf.cell(0, 10, txt=cards[index][0], border=0, ln=True, align='C')
+        pdf[f"textq{index+1}"] = cards[index][0]
     pdf.add_page()
     pdf["logo"] = "logo.png"
     swap_map = {0: 1, 1: 0, 2: 3, 3: 2, 4: 5, 5: 4, 6: 7, 7: 6}
     for index in range(min(8, len(cards))):
         swapped_index = swap_map[index]
-        pdf.set_font("Arial", size=14)
-        pdf.set_xy(pdf.get_x() + (pdf.epw - pdf.get_string_width(cards[swapped_index][1])) / 2, pdf.get_y() + 30)
-        pdf.cell(0, 10, txt=cards[swapped_index][1], border=0, ln=True, align='C')
+        pdf[f"textq{index+1}"] = cards[swapped_index][1]
     pdf.render("test.pdf")
     pdf_output = BytesIO()
     pdf_output.write(pdf.pdf.output(dest='S').encode('latin1'))
     pdf_output.seek(0)
     return pdf_output
-
-
-
