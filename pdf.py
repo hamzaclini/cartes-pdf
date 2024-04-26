3c77c6a8c07c174bf0bf496cf126449942b7bcf6
khribechbouchaib <khribech.chouaib@gmail.com>
2024-04-26 14:45:02 +0200

    commit bouchaib
    ajouter text area au lieu de text input

--- pdf.py
+++ pdf.py
@@ -3,16 +3,15 @@ from fpdf import FPDF
 from io import BytesIO
 import create_pdf
 
-
 st.title("Sequential FlashCard Generator")
 st.subheader("Entrez 8 questions et 8 réponses pour générer les flashcards.")
 
 with st.form(key='cards_form'):
     col1, col2 = st.columns([0.5, 0.5], gap="small")
     with col1:
-        questions = [st.text_input(f"Question:", value=f'Q{i+1} : ') for i in range(8)]
+        questions = [st.text_area(f"Question:", value=f'Q{i+1} : ', height=100) for i in range(8)]
     with col2:
-        answers = [st.text_input(f"Réponses:", value=f'R{i+1} : ') for i in range(8)]
+        answers = [st.text_area(f"Réponses:", value=f'R{i+1} : ', height=100) for i in range(8)]
     submitted = st.form_submit_button("Ajouter les cartes")
 
     if submitted:
@@ -42,4 +41,3 @@ else:
     for q, a in st.session_state["cards"]:
         st.write(f'Question : {q}')
         st.write(f'Réponse : {a}')
-
