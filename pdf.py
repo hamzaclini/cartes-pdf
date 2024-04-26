91ffc960757b41a5d1f743fbe6d2f7ab4160dcb1
csdevignes <159613482+csdevignes@users.noreply.github.com>
2024-04-26 11:44:17 +0200

    French translation

--- pdf.py
+++ pdf.py
@@ -5,37 +5,37 @@ import create_pdf
 
 
 st.title("Anki-style Flashcard Generator")
-st.subheader("Enter 8 questions and 8 answers to generate flashcards.")
+st.subheader("Entrez 8 questions et 8 réponses pour générer les flashcards.")
 
 with st.form(key='cards_form'):
     col1, col2 = st.columns([0.5, 0.5], gap="small")
     with col1:
-        questions = [st.text_input(f"Question {i+1}:", value=f'Q{i+1}') for i in range(8)]
+        questions = [st.text_input(f"Question:", value=f'Q{i+1}') for i in range(8)]
     with col2:
-        answers = [st.text_input(f"Answer {i+1}:", value=f'A{i+1}') for i in range(8)]
-    submitted = st.form_submit_button("Add to Cards")
+        answers = [st.text_input(f"Réponses:", value=f'R{i+1}') for i in range(8)]
+    submitted = st.form_submit_button("Ajouter les cartes")
 
     if submitted:
         if 'cards' not in st.session_state:
             st.session_state['cards'] = []
         for question, answer in zip(questions, answers):
             st.session_state['cards'].append((question, answer))
-        st.success("Cards added! You can add more or generate the PDF.")
+        st.success("Cartes ajoutées !")
 
-if st.button("Generate PDF"):
+if st.button("Générer le PDF"):
     if 'cards' in st.session_state and len(st.session_state['cards']) >= 8:
         pdf_file = create_pdf.create_pdf(st.session_state['cards'])
-        st.success("PDF Generated! You can now download it.")
-        st.download_button(label="Download PDF",
+        st.success("PDF généré. Vous pouvez maintenant le télécharger.")
+        st.download_button(label="Téléchargement PDF",
                            data=pdf_file,
                            file_name="anki_cards.pdf",
                            mime='application/pdf')
     else:
-        st.error("Please add enough cards to generate a PDF (at least 8).")
-if st.button("Clear"):
+        st.error("Ajoutez suffisamment de cartes pour générer un PDF (min 8).")
+if st.button("Effacer les cartes"):
     st.session_state.clear()
 
-st.header("Preview")
+st.header("Aperçu")
 if 'cards' not in st.session_state:
     st.write('Cards cleared.')
 else:
