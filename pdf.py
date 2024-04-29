67d12704fd55c115ea2bb2e966eb0094aedeab9a
khribechbouchaib <khribech.chouaib@gmail.com>
2024-04-29 11:39:36 +0200

    commit
    change messages en fr

--- pdf.py
+++ pdf.py
@@ -3,8 +3,8 @@ from fpdf import FPDF
 from io import BytesIO
 import create_pdf
 
-st.title("Sequential FlashCard Generator")
-st.subheader("Entrez 8 questions et 8 réponses pour générer les flashcards.")
+st.title("générateur de cartes")
+st.subheader("Entrez les questions et les réponses pour générer les cartes")
 
 with st.form(key='cards_form'):
     col1, col2 = st.columns([0.5, 0.5], gap="small")
@@ -25,12 +25,12 @@ if st.button("Générer le PDF"):
     if 'cards' in st.session_state and len(st.session_state['cards']) >= 8:
         pdf_file = create_pdf.create_pdf(st.session_state['cards'])
         st.success("PDF généré. Vous pouvez maintenant le télécharger.")
-        st.download_button(label="Téléchargement PDF",
+        st.download_button(label="Télécharger PDF",
                            data=pdf_file,
-                           file_name="seq_flashcards.pdf",
+                           file_name="cartes.pdf",
                            mime='application/pdf')
     else:
-        st.error("Ajoutez suffisamment de cartes pour générer un PDF (min 8).")
+        st.error("Ajouter suffisamment de cartes pour générer un PDF.")
 if st.button("Effacer les cartes"):
     st.session_state.clear()
 
